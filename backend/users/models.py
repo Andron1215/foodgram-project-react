332582d89abd491e2908806e5c739521c2a8275f
Andrey <andrey.bogidaev03@gmail.com>
2023-11-11 14:23:27 +0300

    Fix User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,6 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
