c9a0d831065496185488989f5b66d3cd46d1e345
Andrey <andrey.bogidaev03@gmail.com>
2023-11-11 14:33:09 +0300

    Add SubscribeAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,5 +3,12 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import Subscription, User
 
+
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ["id", "author", "user"]
+    list_display_links = ["id", "user", "author"]
+    search_fields = ["user", "author"]
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Subscription)
+admin.site.register(Subscription, SubscribeAdmin)
